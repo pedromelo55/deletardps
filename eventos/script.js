9b35079e53d6d9c585f5b56422ad397160e0d266
Pedro Ivo Santana Melo <pedro.smelo@goias.gov.br>
2022-12-20 07:44:39 -0300

    files added

--- eventos/script.js
+++ eventos/script.js
@@ -14,3 +14,22 @@ input.onkeydown = function() {
 
 const h1 = document.querySelector('h1');
 h1.addEventListener('mouseover', print)
+
+// h1.onclick = print /*Essa maneira so executa a última que encontrar*/
+
+
+/* Executar 2
+
+const h1 = document.querySelector('h1');
+h1.addEventListener('click', print)
+
+function print() {
+    console.log('print')
+}
+
+h1.addEventListener('click', function) {
+    console.log('outro momento')
+}
+
+
+*/
